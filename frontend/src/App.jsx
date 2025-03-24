6e7c49709960a3bd419f08e84aa0e42bddc5db27
Jay Prajapati <jay@Jays-MacBook-Air.local>
2025-03-24 14:51:05 +0530

    m

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/")
+    fetch("http://backend:8000/")
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch((err) => console.error(err));
